dca78f42253605e949a934e59defeede9b0e2281
Rodrigo de Oliveira Carvalho Soares <rodrigo.soares@expocaccer.net>
2022-01-07 22:10:58 -0300

    FEAT: Desenvolvimento dos componentes iniciais e Header/Footer da aplicação

---
+++ src/pages/index.tsx
@@ -0,0 +1,13 @@
+import { GetStaticProps } from "next";
+
+export const getStaticProps: GetStaticProps = async () => {
+  return {
+    props: {
+      title: "",
+    },
+  };
+};
+
+export default function Index() {
+  return <div></div>;
+}
